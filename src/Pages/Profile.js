c6bc5e9c56d6229e85f8e62c1c4361230d2cb8eb
Sara-frontend-developer <sajjadnia.sara@gmail.com>
2023-08-17 15:30:27 +0800

    Edit Profile Page

--- src/Pages/Profile.js
+++ src/Pages/Profile.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import Header from '../Components/Header'
-import imageHeader from '../assets/images/slide1.jpeg'
+import imageHeader from '../assets/images/Header-shop.jpg'
 export default function Profile() {
   return (
     <><Header imageSrc={imageHeader}  /></>
